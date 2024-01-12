20b22b83089b9c0dae5513fedb35d71104bd1c92
Hamid47hrs <hamid47.hrs@gmail.com>
2024-01-12 19:56:11 +0330

    Install "Router" and implement routes.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import Home from "./containers/home/Home";
 import Shopping from "./containers/shopping/Shopping";
+import Checkout from "./containers/checkout/Checkout";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Layout>
-          <p>Shopping</p>
-          <Shopping />
-        </Layout>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/store" exact element={<Shopping />} />
+              <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+          </Layout>
+        </Router>
       </div>
     </div>
   );
