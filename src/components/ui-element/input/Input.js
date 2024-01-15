655229d302cf2d38825abc1152a1b6021e405cdf
Hamid47hrs <hamid47.hrs@gmail.com>
2024-01-15 03:44:25 +0330

    Create 'Input' component.

---
+++ src/components/ui-element/input/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./input.css";
+
+const Input = (props) => {
+  return (
+    <div className="input-container">
+      <label
+        className={`input-label ${props.isInvalid ? "invalid" : null}`}
+        htmlFor={props.id}
+      >
+        {`${props.label} :`}
+      </label>
+      <input
+        className={`text-input ${
+          props.isInvalid ? "invalid invalid-input" : null
+        }`}
+        type={props.type}
+        id={props.id}
+        name={props.name}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.change}
+        onFocus={props.focus}
+        onBlur={props.blur}
+      />
+    </div>
+  );
+};
+
+export default Input;
